e80daa5529843b550601b8a0bd2ba7c7af4b7eb5
aymericschaeffer <schaeffer.aymeric@gmail.com>
2023-12-17 14:43:02 +0100

    Importation du module gestionnaire_affichage.py

--- analyse_cge/affichage/gestionnaire_affichage.py
+++ analyse_cge/affichage/gestionnaire_affichage.py
@@ -13,5 +13,44 @@
 #  ==============================================================================
 
 import matplotlib.pyplot as plt
-def affichage():
+
+
+def affichage_X_Y(X, Y):
+    """
+    Cette fonction affiche un graph linéaire.
+
+    Args:
+        X (list): Valeurs abscisse
+        Y (list): Valeurs ordonnées
+    """
+
     return
+
+
+def affichage_bar(X, Y):
+    """
+    Cette fonction affiche un diagramme en barre à partir de listes X et Y
+
+    Args:
+        X (list): Valeurs abscisse
+        Y (list): Valeurs ordonnées
+    """
+
+    return
+
+
+def affichage_pie(X, Y, titre):
+    """
+    Cette fonction affiche un diagramme en camembert à partir d'une liste X
+
+    Args:
+        X (list): Liste des valeurs à représenter
+        Y (list): Liste des labels
+        titre (string): Titre du graphique
+    """
+
+    # On importe un modèle pour créer des graphiques camembert
+    plt.pie(X, labels=Y, autopct='%1.1f%%')
+    plt.title(titre)
+    plt.show()
+    plt.close()
